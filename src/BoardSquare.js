391cacacfc7c8af39178f0a0f8ae10c84d98ee5f
Tim Verheijke <t.verheijke@gmail.com>
2016-02-12 10:36:32 +0100

    boardSquare is now a DropTarget

--- src/BoardSquare.js
+++ src/BoardSquare.js
@@ -1,22 +1,67 @@
 import React, { Component, PropTypes } from 'react';
 import Square from './Square';
+import { canMoveKnight, moveKnight } from './Game';
+import { ItemTypes } from './Constants';
+import { DropTarget } from 'react-dnd';
+
+const squareTarget = {
+    canDrop(props) {
+        return canMoveKnight(props.x, props.y);
+    },
+    drop(props) {
+        moveKnight(props.x, props.y);
+    }
+};
+
+function collect(connect, monitor) {
+    return {
+        connectDropTarget: connect.dropTarget(),
+        isOver: monitor.isOver(),
+        canDrop: monitor.canDrop()
+    };
+}
 
 class BoardSquare extends Component {
+    _renderOverlay(color) {
+        return (
+            <div style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                zIndex: 1,
+                opacity: 0.5,
+                backgroundColor: color
+            }} />
+        )
+    }
     render () {
-        const { x, y } = this.props;
+        const { x, y, connectDropTarget, isOver, canDrop } = this.props;
         const black = (x + y) % 2 === 1;
 
-        return (
-            <Square black={black}>
-                {this.props.children}
-            </Square>
+        return connectDropTarget(
+            <div style={{
+                position: 'relative',
+                width: '100%',
+                height: '100%'
+            }}>
+                <Square black={black}>
+                    {this.props.children}
+                </Square>
+                {isOver && !canDrop && this._renderOverlay('red')}
+                {!isOver && canDrop && this._renderOverlay('yellow')}
+                {isOver && canDrop && this._renderOverlay('green')}
+            </div>
         );
     }
 }
 
 BoardSquare.propTypes = {
     x: PropTypes.number.isRequired,
-    y: PropTypes.number.isRequired
+    y: PropTypes.number.isRequired,
+    isOver: PropTypes.bool.isRequired,
+    canDrop: PropTypes.bool.isRequired
 };
 
-export default BoardSquare;
+export default DropTarget(ItemTypes.KNIGHT, squareTarget, collect)(BoardSquare);
